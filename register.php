2aa067049f21d52bd851631a93f0f131e7bd9acd
James Nearn <github@nearn.org>
2014-01-19 20:07:05 -0500

    Adding client-side password validation

--- register.php
+++ register.php
@@ -1,8 +1,8 @@
 <?php ob_start(); ?>
 
 	<style>
-		.register .content .email,
-		.register .content .password
+		.register .content #email,
+		.register .content #password
 		{
 			width: 100%;
 		}
@@ -15,11 +15,46 @@
 			<div class="content">
 				<div class="contentItem">
 					<div>Email:</div>
-					<input class="email" type="email" name="email" placeholder="username@domain.com" autofocus><br />
+					<input id="email" type="email" name="email" placeholder="username@domain.com" autofocus><br />
 				</div>
 				<div class="contentItem">
 					<div>Password:</div>
-					<input class="password" type="password" name="password"><br />
+					<input id="password" type="password" name="password"><br />
+					<div id="requirements">
+						Requirements:
+						<ul>
+							<li id="CharsLength">At least 10 characters</li>
+							<li id="Numbers">Include at least one number</li>
+							<li id="Specials">Include at least one special character</li>
+						</ul>	
+					</div>
+					<script>
+						$('#password').keyup(function () {
+							var pass = $(this).val();
+							$("#requirements").show();
+							
+							if ($(this).val().length < 10) {
+								$("#CharsLength").css('color', 'red');
+							} else {
+								$("#CharsLength").css('color', 'green');
+							}
+							
+							if (pass.match('[0-9]') == null) {
+								$("#Numbers").css('color', 'red');
+							} else {
+								$("#Numbers").css('color', 'green');
+							}
+
+							if (pass.match('[!@#$%*]') == null) {
+								$("#Specials").css('color', 'red');
+							} else {
+								$("#Specials").css('color', 'green');
+							}
+						});
+						$(document).ready(function () {
+							$("#requirements").hide();
+						});
+					</script>
 				</div>
 				<input type="submit" value="Log In">
 			</div>
